27d4e40135e22ffef974b009c2ac7a1fc5a59e94
GZ MIS - Edison Li (李明斌) <edison.li@baleno.network>
2018-01-22 15:36:54 +0800

    Chapter 13

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 import os
 
 app = Flask(__name__)
@@ -15,57 +16,46 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 login = LoginManager(app)
 login.login_view = 'login'
 
 if not app.debug:
 
-#email logger
-	if app.config['MAIL_SERVER']:
+    # email logger
+    if app.config['MAIL_SERVER']:
 
-		auth = None
-		if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-			auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
 
-		secure = None
-		if app.config['MAIL_USE_TLS']:
-			secure = ()
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
 
-		mail_handler = SMTPHandler(
-			mailhost = (app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-			fromaddr = 'no-reply@' + app.config['MAIL_SERVER'],
-			toaddrs = app.config['ADMINS'],
-			sunject = 'Microblog Failure',
-			credentials=auth,
-			secure = secure)
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'],
+            sunject='Microblog Failure',
+            credentials=auth,
+            secure=secure)
 
-		mail_handler.setLevel(logging.ERROR)
-		app.logger.addHandler(mail_handler)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
 
-#file logger
+# file logger
 
 if not os.path.exists('logs'):
-	os.mkdir('logs')
-file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    os.mkdir('logs')
+file_handler = RotatingFileHandler(
+    'logs/microblog.log', maxBytes=10240, backupCount=10)
 file_handler.setFormatter(logging.Formatter(
-			'%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
 file_handler.setLevel(logging.INFO)
 app.logger.addHandler(file_handler)
 
 app.logger.setLevel(logging.INFO)
-app.logger.info('Microblog startup') 
+app.logger.info('Microblog startup')
 
 from app import routes, models, errors
-
-
-
-
-
-
-
-
-
-
-
-
-
